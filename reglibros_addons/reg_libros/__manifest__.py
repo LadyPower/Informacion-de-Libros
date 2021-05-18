5533769fd656165887704ea0e37d8dfeb079c563
LadyPower <sirius.power@hotmail.com>
2021-05-18 14:12:53 -0400

    init

---
+++ reglibros_addons/reg_libros/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name":"Información de Libros",
+    "description": "Aplicación que permite tener un registro de información de diversos libros, autores y colecciones en una tienda.",
+    "depends":['base'],
+    "data":[
+     'views/views.xml',
+     'security/security.xml'   
+    ]
+}
---
+++ reglibros_addons/reg_libros/models/models.py
@@ -0,0 +1,26 @@
+from odoo import models,fields
+
+class Libros(models.Model):
+    _name = "reglib.libros"
+
+    name = fields.Char("Título del libro",required = True)
+    autor_id = fields.Many2one('reglib.autores',required = True)
+    name_e = fields.Char("Editorial",required = True)
+    num_e = fields.Integer("Edición")
+    publ = fields.Integer("Año de publicación")
+    status = fields.Selection(selection=[("disponible","Disponible"),("agotado","Agotado")],required = True)
+    pertc = fields.Boolean("¿Pertenece a una colección?")
+    coleccion_id = fields.Many2one('reglib.coleccion', string="Colección")
+    num_t = fields.Integer("Número de tomo")
+
+class Autores(models.Model):
+    _name = "reglib.autores"
+
+    name = fields.Char("Autor del libro", required = True)
+    libros_ids = fields.One2many('reglib.libros', inverse_name='autor_id', string="Libros")
+
+class Coleccion(models.Model):
+    _name = "reglib.coleccion"
+
+    name = fields.Char("Nombre de la colección", required = True)
+    libros_ids = fields.One2many('reglib.libros', inverse_name='coleccion_id', string="Libros")
