860eb6d34e693378a915fc31cead237c2a1158dc
Sirius Power <sirius.power@hotmail.com>
2021-05-18 15:47:34 -0400

    init

--- reglibros_addons/reg_libros/__manifest__.py
+++ reglibros_addons/reg_libros/__manifest__.py
@@ -1,6 +1,7 @@
 {
     "name":"Información de Libros",
     "description": "Aplicación que permite tener un registro de información de diversos libros, autores y colecciones en una tienda.",
+    "author": "Sirius Power",
     "depends":['base'],
     "data":[
      'views/views.xml',
